6cc46e9ec6741ba75c8e3d7449c65838ee50b8c4
YuriyShorin <ushorin.03@gmail.com>
2025-05-24 18:32:04 +0300

    feat: Micran connection

--- src/main/java/ru/hse/objectsmeterapp/utils/StringUtils.java
+++ src/main/java/ru/hse/objectsmeterapp/utils/StringUtils.java
@@ -1,22 +1,11 @@
 package ru.hse.objectsmeterapp.utils;
 
-import java.util.List;
-
 public class StringUtils {
 
-    public static String clearBlanks(String line) {
-        return line
-                .replaceAll("\\s+", " ")
-                .trim();
-    }
-
-    public static String clear(String line, String target) {
-        return line
-                .replace(target, "")
-                .trim();
-    }
-
-    public static List<String> split(String line) {
-        return List.of(line.split("\\s"));
+    public static String stringOrDefault(String str, String defaultStr) {
+        if (str == null || str.isEmpty() || str.isBlank()) {
+            return defaultStr;
+        }
+        return str;
     }
 }
